e1dce46a6b126eae8bef1400aa3ebf5105e479ff
sh0la <sholaadeniyi@gmail.com>
2019-12-16 11:22:06 -0600

    Add function to handle AJAX success

--- js/app.js
+++ js/app.js
@@ -1,9 +1,39 @@
 const input = document.querySelector('#movie-name');
 const form = document.querySelector('form');
+const url = 'http://www.omdbapi.com/?'
+const apikey = 'apikey=c4fc44ed'
 
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  const userQuery = input.value
+  const userQuery = '&s=' + input.value;
+
+  const searchUrl = `${url}${apikey}${userQuery}`
+
+  //Using the AJAX function
+  
+  const xhr = new XMLHttpRequest();
+  //xhr.response = 'json';
+  xhr.open('GET', searchUrl)
+  console.log(searchUrl)
+
+  const handleSuccess = function() {
+    console.log(xhr.responseText)
+    let data = JSON.parse(xhr.responseText)
+
+    for (each of data.Search) {
+      form.insertAdjacentHTML('afterend', `<img src=${each.Poster}>`)
+      //console.log(`${each.Poster}/n`)
+    }
+    //console.log(data)
+   
+  }
+
+  
+  xhr.onload = handleSuccess;
+  xhr.send();
+
+
+
 })
 
