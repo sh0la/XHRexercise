202632d27e7ccad5e5b61de8ba80b4a8c8db8eef
sh0la <sholaadeniyi@gmail.com>
2019-12-16 10:26:41 -0600

    Add eventlistener to listen to submit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#movie-name');
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const userQuery = input.value
+})
+
